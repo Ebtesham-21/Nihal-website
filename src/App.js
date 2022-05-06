384cd6b3003387aa7229c5bdadde71b99547f95f
md ezaz <mdezaz@mds-MacBook-Pro.local>
2022-05-07 03:38:09 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import Container from 'react-bootstrap/Container';
-import Navigation from './Navbar/Navigation';
-import Banner from './Banner/Banner';
+import Navigation from './components/Shared/Navbar/Navigation';
+import Banner from './components/Home/Banner/Banner';
+import Search from './components/Home/Search/Search';
+import Featured from './components/Home/Featured Properties/Featured';
 import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Recentproperties from './components/Home/Recent Properties/Recentproperties';
+import Whychooseus from './components/Home/Whychooseus/Whychooseus';
+import Mostpopular from './components/Home/Most Popular/Mostpopular';
+import Partner from './components/Home/Partner/Partner';
+import Whatpeople from './components/Home/WhatPeople/Whatpeople';
+import Signup from './components/Home/Signup/Signup';
+import Latestnews from './components/Home/Latestnews/Latestnews';
 
 
 
@@ -14,6 +23,15 @@ function App() {
     <Container>
       <Navigation />
       <Banner />
+      <Search />
+      <Featured />
+      <Recentproperties />
+      <Mostpopular />
+      <Whychooseus />
+      <Partner />
+      <Whatpeople />
+      <Signup />
+      <Latestnews />
 
     </Container>
 
---
+++ src/components/Home/Partner/Partner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Partner = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>Our Partners</Col>
+
+            </Row>
+            <Row>
+                <Col>Brand Partners Successful Projects Trusted Many Clients Real Estate</Col>
+
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+                <Col>3 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Partner;
